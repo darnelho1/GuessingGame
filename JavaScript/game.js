c845a093c98dbd53ef0e885b5cb2b66ce6f1654e
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2015-11-11 21:51:50 -0800

    add stylesheet

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,9 +1,10 @@
 
+
 var scoreIt = 0; // running tally of correct answers
 
-var askToPlay = function(){
+var toPlay = function(){
   userName=prompt("What is your name?");
-  confirm("Welcome to my guessing game " + userName + " would you like to play? (Press ok to play the game)");
+  confirm("Welcome to my guessing game " + userName + ". Let's play! (Press ok to play the game)");
 }();
 
 // Put Questions Into Arrays
@@ -37,10 +38,12 @@ var question1 = function() {
   var userAnswer1 = prompt(arrayQuestions[0]);
   if((userAnswer1.toUpperCase() === "BOSTON") || (userAnswer1.toUpperCase() === "BOS")) {
     document.getElementById('question1').innerHTML= arrayAnswerCorrect[0];
+    document.getElementById('question1').style.color = "green";
     document.getElementById('img1').src = "images/correct.png";
     scoreIt++;
   }else {
     document.getElementById('question1').innerHTML= arrayAnswerIncorrect[0];
+    document.getElementById('question1').style.color = "red";
     document.getElementById('img1').src = "images/incorrect.png";
   };
 }();//IIFE
@@ -50,10 +53,12 @@ var question2 = function() {
   var userAnswer2 = prompt(arrayQuestions[1]);
   if((userAnswer2.toUpperCase() === "OCTOBER")||(userAnswer2.toUpperCase() === "OCT")) {
     document.getElementById('question2').innerHTML=arrayAnswerCorrect[1];
+    document.getElementById('question2').style.color = "green";
     document.getElementById('img2').src = "images/correct.png";
     scoreIt++;
   }else {
     document.getElementById('question2').innerHTML=arrayAnswerIncorrect[1];
+    document.getElementById('question2').style.color = "red";
     document.getElementById('img2').src = "images/incorrect.png";
   };
 }();
@@ -62,10 +67,12 @@ var question3 = function() {
   var userAnswer3 = prompt(arrayQuestions[2]);
   if((userAnswer3 === "7")||(userAnswer3.toUpperCase() === "seven")) {
     document.getElementById('question3').innerHTML=arrayAnswerCorrect[2];
+    document.getElementById('question3').style.color = "green";
     document.getElementById('img3').src = "images/correct.png";
     scoreIt++;
   }else {
     document.getElementById('question3').innerHTML=arrayAnswerIncorrect[2];
+    document.getElementById('question3').style.color = "red";
     document.getElementById('img3').src = "images/incorrect.png";
   };
 }();
@@ -74,10 +81,12 @@ var question4 = function() {
   var userAnswer4 = Number(prompt(arrayQuestions[3]));
   if(userAnswer4 === 32) {
     document.getElementById('question4').innerHTML=arrayAnswerCorrect[3];
+    document.getElementById('question4').style.color = "green";
     document.getElementById('img4').src = "images/correct.png";
     scoreIt++;
   }else {
     document.getElementById('question4').innerHTML=arrayAnswerIncorrect[3];
+    document.getElementById('question4').style.color = "red";
     document.getElementById('img4').src = "images/incorrect.png";
   };
 }();
@@ -86,10 +95,12 @@ var question5 = function() {
   var userAnswer5 = prompt(arrayQuestions[4]);
   if ((userAnswer5 === '11') || (userAnswer5.toUpperCase() ==='ELEVEN')) {
     document.getElementById('question5').innerHTML=arrayAnswerCorrect[4];
+    document.getElementById('question5').style.color = "green";
     document.getElementById('img5').src = "images/correct.png";
     scoreIt++;
   }else {
     document.getElementById('question5').innerHTML=arrayAnswerIncorrect[4];
+    document.getElementById('question5').style.color = "red";
     document.getElementById('img5').src = "images/incorrect.png";
   };
 }();
@@ -106,12 +117,15 @@ var bonusRound = function() {
        var guess = Number(prompt(arrayQuestions[5]));
         if(guess > numberRandom) {
           document.getElementById('bonusA').innerHTML=arrayAnswerIncorrect[5];
+          document.getElementById('bonusA').style.color = "red";
           document.getElementById('imgB').src = "images/incorrect.png";
         }else if (guess < numberRandom) {
           document.getElementById('bonusA').innerHTML=arrayAnswerIncorrect[6];
+          document.getElementById('bonusA').style.color = "red";
           document.getElementById('imgB').src = "images/incorrect.png";
         }else {
           document.getElementById('bonusA').innerHTML=arrayAnswerCorrect[5];
+          document.getElementById('bonusA').style.color = "green";
           document.getElementById('imgB').src = "images/correct.png";
           result=0;
     };
